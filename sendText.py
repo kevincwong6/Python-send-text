ab43bbc7f899a38875aadf8be0a5dff1d1879898
Kevin Wong <48312245+kevincwong6@users.noreply.github.com>
2019-04-18 02:00:56 -0700

    replaced tab with white spaces

--- sendText.py
+++ sendText.py
@@ -21,7 +21,7 @@ def getCarrierGateway(carrier):
   switcher = {
     0: "txt.att.net",       # AT&T
     1: "pm.sprint.com",     # Sprint
-	2: "tmomail.net",       # T-Mobile
+    2: "tmomail.net",       # T-Mobile
     3: "vtext.com",         # Verizon
     4: "myboostmobile.com", # Boost Mobile
     5: "sms.mycricket.com", # Cricket
